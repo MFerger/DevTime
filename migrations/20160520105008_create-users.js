2d93234d5a176c01dd9cb1a9eba1d9eb16cb0d98
Wfuger <wfuger@gmail.com>
2016-05-20 14:03:11 -0600

    wip

--- migrations/20160520105008_create-users.js
+++ migrations/20160520105008_create-users.js
@@ -2,9 +2,9 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', table => {
             table.increments();
-            table.string('phone').notNullable();
+            table.string('phone').notNullable().unique();
             table.string('twilio_id').notNullable().unique();
-            table.string('token').notNullable();
+            table.string('token').notNullable().unique();
             table.string('name').notNullable().unique();
             table.integer('q1');
             table.integer('q2');
